907786df36953d8e78c42770778cbd2a035c178b
Federico <federico.b.giordani@gmail.com>
2023-02-01 14:57:46 +0100

    General changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express, { Express } from 'express';
 import http, { Server } from 'http';
 import { WebSocketServer, Server as WSServer } from 'ws';
-import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app: Express = express();
 const server: Server = http.createServer(app);
 const wss: WSServer = new WebSocketServer({ server });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    console.log(data.toString());
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log('Server is listening on port 3000');
+});
